85126176cd02d75572d29f23a6a5f98dc1299279
AtomoiC <129673899+AtomoiC@users.noreply.github.com>
2023-04-16 21:48:12 +0200

    Added AppSettings for local configuration

--- James/Program.cs
+++ James/Program.cs
@@ -5,6 +5,10 @@ internal class Program
     public static void Main(string[] args)
     {
         IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((ctx, config) =>
+            {
+                config.AddJsonFile($"appsettings.{ctx.HostingEnvironment.EnvironmentName}.local.json", true, true);
+            })
             .ConfigureServices(services => { services.AddHostedService<Worker>(); })
             .Build();
 
